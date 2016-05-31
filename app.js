84fb6ff8e8c5d33c4ca11e40345d9f4e49e28a56
Nurbek Ismailov <nurbek82@gmail.com>
2016-05-31 00:10:41 -0700

    created arr 4 corr n wrong ans

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ var questions = [
  ['Do I live in South Seattle?', 'yes']
 ];
 
+
+
 var correctGuess=0; // counter variable
 var question;
 var answer;
 var response;
 var results;
+var correct= []; // empty array to include list questions got right
+var wrong= [];   // empty array to include list questions got wrong
 
 for (var i = 0; i < questions.length; i++) {
   question= questions[i][0];// accessing the question/1st index i.e 0
@@ -22,16 +26,31 @@ for (var i = 0; i < questions.length; i++) {
   response=(prompt(question)).toLowerCase();
   if (response===answer) {
     correctGuess+= 1;   // if user's  response matches to the correct answer,added to the counter
+    correct.push (question);
+  } else {
+    wrong.push (question);
   }
 }
-
-
+// function=buildList (arr) {
+//   var listHTML="<ol>";
+//   for (var i=0; i <arr.length; i+=1) {
+//     listHTML+= '<li>' + arr[i] + '</li>';
+//   }
+//
+//   listHTML+='</ol>';
+//   return listHTML;
+//   results += '<h2>You got these questions correct:</h2>';
+//   results += buildList(correct);
+//   results += '<h2>You got these questions wrong:</h2>';
+//   results += buildList(wrong);
+//   displayResults(results);
+// }
 
 var question5 = prompt ('Guess what is my favorite number between 1 & 9');
 var guess= 3
 var guessLeft = '[' + guess + ' Guess(es) Left]';
 
-while (guess > 0) {
+while (guess > 0) {       //  while loop to keep prompting 4 times
   if ( parseInt(question5)=== 6) {
     alert ('You got it Right');
     correctGuess+=1;
@@ -49,7 +68,7 @@ while (guess > 0) {
   }
   }
   var statesLived=['montana','california', 'florida', 'oregon'];
-  var answerWasFound=false; // this var helps to alert to You guessed it wrong when it is switched to false
+  var answerWasFound=false; // this var helps to alert- You guessed it wrong when it is switched to false
   var question6= prompt ('What states have I lived in besides Washington?');
     for (var i = 0; i < statesLived.length; i++) {
     if (question6.toLowerCase() === statesLived[i]) {
@@ -67,6 +86,8 @@ while (guess > 0) {
  function displayResults (message) {
    var printHTML= document.getElementById('outcome');
  printHTML.innerHTML=message;
+  }
+
   }
  var results= '<p>You got ' + correctGuess + ' question(s) right';
  if (correctGuess >= 4) { results += "<strong> and You Earned a Gold Medal</strong></p>"; displayResults(results);
