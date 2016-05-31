beb14bdd04209b6ce72d4794e4829508551b0f9a
Nurbek Ismailov <nurbek82@gmail.com>
2016-05-30 17:21:02 -0700

    added array for text qstns.

--- app.js
+++ app.js
@@ -1,86 +1,32 @@
 var userInput= prompt("Welcome to my App, What is Your Name?");
 var welcome= alert ('Hello ' + userInput + ',  let\'s play guessing game, if you guess correctly you will get a medal');
 
-
+//Two dimensional array with text question and answer
 var questions = [
- ['Have I been to Turkey?', 'yes'],
+ ['Have I been to Turkey,?', 'yes'],
  ['Which Sport is my Favorite Sport Tennis or Rugby?', 'tennis'],
- ['Is Aquire my favorite board game?', 'yes']
-
+ ['Is Aquire my favorite board game,?', 'yes'],
+ ['Do I live in South Seattle?', 'yes']
 ];
 
-var correctGuess=0;
+var correctGuess=0; // counter variable
 var question;
 var answer;
 var response;
 var results;
 
-function print (message) {
-  var outcomeId= document.getElementById('outcome');
-  outcomeId.innerHTML=message;
-}
-
 for (var i = 0; i < questions.length; i++) {
-  question= questions[i][0];
-  answer=questions [i][1];
+  question= questions[i][0];// accessing the question/1st index i.e 0
+  answer=questions [i][1]; //accessing the answer/2nd index i.e 1
 
   response=(prompt(question)).toLowerCase();
   if (response===answer) {
-    correctGuess+= 1;
-  }
-}
-results= 'You got ' + correctGuess +  ' question(s) right.';
-print (results);
-
-
-
-var rightAnswer=['montana','california', 'florida', 'oregon'];
-var answerWasFound=false;
-var question6= prompt ('What states have I lived in besides Washington?');
-for (var i = 0; i < rightAnswer.length; i++) {
-  if (question6.toLowerCase() === rightAnswer[i]) {
-    alert('You guessed it right');
-    answerWasFound=true;
-    correctGuess+= 1;
-    break;
-  }
-  else {
-    alert('You guessed it wrong');
-    break;
+    correctGuess+= 1;   // if user's  response matches to the correct answer,added to the counter
   }
 }
 
 
-// var question1 = prompt('Do you think I was born Washington');
-//
-// if (question1.toLowerCase ()==="no" || question1.toLowerCase ()==="n") {
-//   alert ('You got it right');
-//   correctGuess+=1;
-// } else {
-//   alert ('You got it wrong, good luck with the next question');
-// };
-// var question2 = prompt ('Which Sport is my Favorite Sport Tennis or Rugby?');
-// if (question2.toLowerCase ()==='tennis') {
-//   alert ('You got it right');
-//   correctGuess+=1;
-// } else {
-//   alert ('You got it Wrong, good luck with the next question');
-// };
-// // var question3 = prompt ('Guess how many countries have I visited, # between 1 & 8?')
-// //   if (question3.toLowerCase ()==='five' || (question3)==='5') {
-// //     alert ('You got it right');
-// //     correctGuess+=1;
-// //   } else {
-// //     alert ('You got it wrong, good luck with the next question');
-// //   };
-// var question4 = prompt ('Is Aquire my favorite board game?');
-// if (question4.toLowerCase ()==="yes" || question4.toLowerCase ()==="y") {
-//   alert ('You got it right');
-//   correctGuess+=1;
-// } else {
-//   alert ('You got it wrong, good luck with the next question');
-// };
-//
+
 var question5 = prompt ('Guess what is my favorite number between 1 & 9');
 var guess= 3
 var guessLeft = '[' + guess + ' Guess(es) Left]';
@@ -99,23 +45,40 @@ while (guess > 0) {
     guess-=1;
     var guessLeft = '[' + guess + ' Guess(es) Left]';
   } else {
-    alert ('You got it wrong, lets move on to next quiz?');
+    alert ('Sorry, You got it wrong!?');
   }
-}
+  }
+  var statesLived=['montana','california', 'florida', 'oregon'];
+  var answerWasFound=false; // this var helps to alert to You guessed it wrong when it is switched to false
+  var question6= prompt ('What states have I lived in besides Washington?');
+    for (var i = 0; i < statesLived.length; i++) {
+    if (question6.toLowerCase() === statesLived[i]) {
+      alert('You guessed it right');
+      answerWasFound=true;
+      correctGuess+= 1;
+      break;
+    }
+  }
+    if (answerWasFound===false) {
+    alert('You guessed it wrong');
+    }
 
 
- alert ('You got ' + correctGuess + ' out of 6 questions correct')
-
-
-//   if (correctGuess >= 4) {
-// document.write("<p><strong>You Earned a Gold Medal</strong></p>");
-// }
-// else if (correctGuess >= 3) {
-// document.write("<p><strong>You Earned a Silver Medal</strong></p>");
-// }
-// else if (correctGuess >= 2) {
-// document.write("<p><strong>You Earned a Bronze Medal</strong></p>");
-// }
-// else {
-// document.write ('<p><strong>You Earned No Medal</strong></p>');
-// }
+ function displayResults (message) {
+   var printHTML= document.getElementById('outcome');
+ printHTML.innerHTML=message;
+  }
+ var results= '<p>You got ' + correctGuess + ' question(s) right';
+ if (correctGuess >= 4) { results += "<strong> and You Earned a Gold Medal</strong></p>"; displayResults(results);
+  }
+ else if
+ (correctGuess >= 3) { results += "<strong> and You Earned a Silver Medal</strong></p>"; displayResults(results);
+ }
+ else if
+ (correctGuess >= 2) { results += "<strong> and You Earned a Bronze Medal</strong></p>"; displayResults(results);
+ }
+ else
+ {
+  results += "<strong> and You Earned No Medal</strong></p>";
+  displayResults(results);
+ }
